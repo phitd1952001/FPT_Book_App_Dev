f69313ed70fa7704428089fa12e9f6d020ecc49b
Dang Khoi <88148432+dankhoi@users.noreply.github.com>
2022-12-05 16:59:35 +0700

    Done login with Facebook and Google

--- Startup.cs
+++ Startup.cs
@@ -52,6 +52,25 @@ namespace FPT_Book_Khôi_Phi
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
+            services.AddAuthentication()
+                .AddGoogle(googleOptions =>
+                {
+                    IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
+ 
+                    // Thiết lập ClientID và ClientSecret để truy cập API google
+                    googleOptions.ClientId = googleAuthNSection["ClientId"];
+                    googleOptions.ClientSecret = googleAuthNSection["ClientSecret"];
+                    // Cấu hình Url callback lại từ Google (không thiết lập thì mặc định là /signin-google)
+                    googleOptions.CallbackPath = "/dang-nhap-tu-google";
+                })
+                .AddFacebook(facebookOptions => {
+                    // Đọc cấu hình
+                    IConfigurationSection facebookAuthNSection = Configuration.GetSection("Authentication:Facebook");
+                    facebookOptions.AppId = facebookAuthNSection["AppId"];
+                    facebookOptions.AppSecret = facebookAuthNSection["AppSecret"];
+                    // Thiết lập đường dẫn Facebook chuyển hướng đến
+                    facebookOptions.CallbackPath = "/dang-nhap-tu-facebook";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
